e71a3285a1d5cbf645d43704c4ab8bb319c0c379
hpzsr <582254172@qq.com>
2021-03-03 19:28:33 +0800

    11

---
+++ Assets/Scripts/MapItem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapItem : MonoBehaviour
+{
+    public CellScript standCellScript = null;
+
+    private void Awake()
+    {
+        MapItemManager.list_item.Add(this);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    public void setPos(CellScript _cellScript)
+    {
+        standCellScript = _cellScript;
+        standCellScript.setCellType(CellType.DisAble);
+        transform.position = new Vector3(standCellScript.transform.position.x, 1, standCellScript.transform.position.z);
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.transform.CompareTag("Cell"))
+    //    {
+    //        other.GetComponent<Cell>().setCellType(CellType.Able);
+    //    }
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.transform.CompareTag("Cell"))
+    //    {
+    //        other.GetComponent<Cell>().setCellType(CellType.Null);
+    //    }
+    //}
+}
---
+++ Assets/Scripts/MapItemManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapItemManager
+{
+    public static List<MapItem> list_item = new List<MapItem>();
+}
